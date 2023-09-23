046f41dda1344b32d1e5690a0c122e81f225073d
Canyon Smith <canyonfsmith@gmail.com>
2023-09-23 13:29:31 -0600

    updated api/index.py

--- api/index.py
+++ api/index.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask
 
 app = Flask(__name__)
@@ -13,3 +14,14 @@ def about():
 @app.route('/hello')
 def hello():
     return 'its your boy!'
+
+@app.route('/checksite')
+def check_site():
+    try:
+        response = requests.get('https://cars.ksl.com/')
+        if response.status_code == 200:
+            return 'its live'
+        else:
+            return 'its not'
+    except:
+        return 'its not'
